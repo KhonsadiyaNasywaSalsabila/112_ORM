47ed08ab20f044f417d83928e05129579096c48e
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-10-29 12:42:26 +0700

    Integrasi Sequelize dan Sinkronisasi Database

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const db = require('./models');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+db.sequelize.sync()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
